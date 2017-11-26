6b32f7929d13f92353941e9fedca7a7949c92920
Jammoo <jammoo@git.com>
2017-11-26 16:58:47 -0500

    added assignment navigation to text question page but its not functional yet

--- HandyHomework/src/gui/AnswerMultipleChoice.java
+++ HandyHomework/src/gui/AnswerMultipleChoice.java
@@ -62,7 +62,7 @@ public class AnswerMultipleChoice extends JFrame {
 	public AnswerMultipleChoice() {
 		setTitle("HandyHomework - Multiple Choice Question");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 450);
+		setBounds(100, 100, 500, 450);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
