046bf93d26ad95aa04b3f253aacfd5f8e119d14f
Jammoo <jammoo@git.com>
2017-11-27 13:23:29 -0500

    committing work on StudentAnswerTextQuestionTest.java

--- HandyHomework/src/test/StudentAnswerTextQuestionTest.java
+++ HandyHomework/src/test/StudentAnswerTextQuestionTest.java
@@ -2,6 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.util.ArrayList;
 
@@ -19,7 +20,10 @@ import assessment.Assessment;
 import assessment.SelectedAssessment;
 import course.Course;
 import course.SelectedCourse;
+import dao.DbAssessment;
+import dao.DbQuestions;
 import gui.AnswerStudentQuestions;
+import gui.HHSavedAssessments;
 import login.ProfessorLogin;
 import login.SelectedUser;
 import login.StudentLogin;
@@ -29,7 +33,7 @@ public class StudentAnswerTextQuestionTest extends AssertJSwingJUnitTestCase {
 	private FrameFixture window;
 
 	@Override
-	protected void onSetUp() {
+	protected void onSetUp() {		
 		// set up student user
 		StudentLogin student = new StudentLogin(1, "student", "password");
 		SelectedUser.setUser(student);
@@ -39,27 +43,42 @@ public class StudentAnswerTextQuestionTest extends AssertJSwingJUnitTestCase {
 		// set up assessment
 		Assessment assessment = new Assessment(
 				3, "Intro to comp sci", "Title vs name", true, null, (float) 0.99);
-		SelectedAssessment.setAssess(assessment);
 		// set up text question with answer
 		TextQuestion text = new TextQuestion(4, "write answer", "just write answer", 5);
 		TextAnswer forText = new TextAnswer(4, "answer", true); 
-		SelectedAssessment.getAssess().addQuestions(text);
+		//DbAssessment dbA = new DbAssessment();
+		//dbA.insertAssessment("CSCC01H5F", 2, "Software Engineering", null, true, 0);
+		//DbQuestions dbQ = new DbQuestions();
+		//dbQ.insertQuestions(2, "write answer", "just write answer", 5, false, false);
+		//dbQ.insertAnswers(4, true, "answer");
 		//
-		AnswerStudentQuestions frame = GuiActionRunner.execute(() -> new AnswerStudentQuestions());
+		SelectedAssessment.setAssess(assessment);
+		SelectedAssessment.getAssess().addQuestions(text);
+		AnswerStudentQuestions frame = GuiActionRunner.execute(() -> new AnswerStudentQuestions() {
+			protected AnswerStudentQuestions executeInEDT() throws Exception {
+				AnswerStudentQuestions frame = new AnswerStudentQuestions();
+            	frame.setVisible(true);
+            	frame.pack();
+            	return frame;
+            }
+		});
 		frame.setVisible(true);
 		window = new FrameFixture(robot(), frame);
 	}
 	
 	@Test
 	public void studentAnswerCorrect() {
+		
 		window.textBox("txtAns").setText("answer");
 		window.button("btnSubmit").click();
 		window.optionPane().requireVisible().requireMessage("Finished assessment, points earned: 5");
+		tearDown();
 	}
 	
 	@Test
 	public void studentAnswerWrong() {
 		fail("Not yet implemented");
+		tearDown();
 	}
 
 }
